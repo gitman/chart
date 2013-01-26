81f26e2391d84ea7baa14cde1bd64de5c239d286
gitman <https://github.com/gitman>
2013-01-25 18:43:20 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chart::Application.config.secret_token = '3cda48140f51a6d87398018f63a7b8d96d56217532b718fd56a68790218cec4d7ca80e86d0993ee2358457745fa493f7380d61cf28e2f45acd4bc1b52a3995cf'
